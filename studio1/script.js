ae6b8300b7a41fc241c06bc2440183d79998db6b
adamwritescode <adamrosado@gmail.com>
2017-01-15 14:26:55 -0800

    Layout and banner changes
    significant update to banner code, css, some html

---
+++ studio1/script.js
@@ -0,0 +1,26 @@
+// This is a processing (p5.js) script
+// to be used for a web banner animation
+// which does something...tba
+
+var diam = 80;
+
+function setup() {
+    var banner = createCanvas(800, 250);
+    banner.parent('banner');
+}
+
+function draw() {
+    if (mouseIsPressed) {
+        fill(0);
+    } else {
+        fill(255);
+    }
+    ellipse(mouseX, mouseY, diam,  diam);
+}
+
+function mouseWheel(event) {
+    print(event.delta);
+    diam += event.delta;
+    print('diam: ' + diam);
+    //return false;
+}
