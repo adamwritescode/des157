6d912090a994f912e0ed628f3ae1f012e7eaf46b
adamwritescode <adamrosado@gmail.com>
2017-01-30 20:15:10 -0800

    Modified a submit button for testing. Primarily working on core functionality in js

--- studio2/script.js
+++ studio2/script.js
@@ -3,6 +3,40 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
-    // all other js here
+    // Declare & initialize form name variables:
+    // -- Power Metal form
+    var powerMetal = document.power;
+
+    // -- Death Metal form
+    var deathMetal = document.death;
+
+    // -- Black Metal form
+    var blackMetal = document.black;
+
+    // -- Hardcore form
+    var hardcore = document.hardcore;
+
+    // bind procesForm() to
+    // call processForm() when submit button is clicked
+    powerMetal.onsubmit = processForm;
+
+    // define processForm()
+    function processForm() {
+      var power_timeOfDay0 = document.power.timeOfDay0.value;
+      console.log(power_timeOfDay0);
+
+      var power_timeOfDay0_span = document.getElementById("power-timeOfDay0");
+      console.log(power_timeOfDay0_span);
+
+      console.log("before " + power_timeOfDay0_span.innerHTML);
+      power_timeOfDay0_span.innerHTML = power_timeOfDay0;
+      console.log("after " + power_timeOfDay0_span.innerHTML);
+
+
+
+
+
+      return false;
+    }
 
 });
