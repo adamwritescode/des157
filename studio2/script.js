626f65b96136a89ebc5d13096d823876f34ead15
adamwritescode <adamrosado@gmail.com>
2017-01-30 23:08:37 -0800

    major changes to core functionality (js structure) + some id/class changes in HTML
    went from manually initializing each variable to initializing with
    loops + DOM traversal.

--- studio2/script.js
+++ studio2/script.js
@@ -16,26 +16,92 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // -- Hardcore form
     var hardcore = document.hardcore;
 
-    // bind procesForm() to
-    // call processForm() when submit button is clicked
+    // call processForm() when submit buttons are clicked
+    // -- Power Metal submit
     powerMetal.onsubmit = processForm;
+
+    // -- Death Metal submit
+    deathMetal.onsubmit = processForm;
+
+    // -- Black Metal submit
+    blackMetal.onsubmit = processForm;
+
+    // -- Hardcore submit
+    hardcore.onsubmit = processForm;
 
     // define processForm()
     function processForm() {
+
+      var powerMetalFields = powerMetal.getElementsByTagName("input");
+      var powerMetalSpans = document.getElementById("power-results").getElementsByTagName("span");
+      for (var i = 0; i < powerMetalFields.length; i++){
+        powerMetalSpans[i].innerHTML = powerMetalFields[i].value;
+      }
+
+      /*
+      // Initialize variables for every text field in each form
+      // as well as their corresponding <span> tags to be populated onsubmit.
+      // To avoid repetitive commenting, the order of var intitialization
+      // is as follows: input value, corresponding <span>, span innerHTML.
+      // Each var name relates to the 'category' of word the user will enter.
+
+      // -- Power Metal
+      // ---- "Time of day"
       var power_timeOfDay0 = document.power.timeOfDay0.value;
-      console.log(power_timeOfDay0);
-
       var power_timeOfDay0_span = document.getElementById("power-timeOfDay0");
-      console.log(power_timeOfDay0_span);
-
-      console.log("before " + power_timeOfDay0_span.innerHTML);
       power_timeOfDay0_span.innerHTML = power_timeOfDay0;
-      console.log("after " + power_timeOfDay0_span.innerHTML);
+
+      // ---- "Time of day"
+      var power_timeOfDay1 = document.power.timeOfDay1.value;
+      var power_timeOfDay1_span = document.getElementById("power-timeOfDay1");
+      power_timeOfDay1_span.innerHTML = power_timeOfDay1;
+
+      // ---- "Planet name"
+      var power_planet0 = document.power.planet0.value;
+      var power_planet0_span = document.getElementById("power-planet0");
+      power_planet0_span.innerHTML = power_planet0;
+
+      // ---- "Mythological creatures (plural)"
+      var power_mythCreature0 = document.power.mythCreature0.value;
+      var power_mythCreature0_span = document.getElementById("power-mythCreature0");
+      power_mythCreature0_span.innerHTML = power_mythCreature0;
+
+      // ---- "Medieval weaponry (plural)"
+      var power_medievalWeapon0 = document.power.medievalWeapon0.value;
+      var power_medievalWeapon0_span = document.getElementById("power-medievalWeapon0");
+      power_medievalWeapon0_span.innerHTML = power_medievalWeapon0;
+
+      // ---- "Medieval weaponry (plural)"
+      var power_medievalWeapon1 = document.power.medievalWeapon1.value;
+      var power_medievalWeapon1_span = document.getElementById("power-medievalWeapon1");
+      power_medievalWeapon1_span.innerHTML = power_medievalWeapon1;
+
+      // ---- "Animals (plural)"
+      var power_animal0 = document.power.animal0.value;
+      var power_animal0_span = document.getElementById("power-animal0");
+      power_animal0_span.innerHTML = power_animal0;
+
+      // each form is an object. Each object has a form, a set of form fields,
+      // a set of associated span tags, and a method which assigns the value of
+      // each field to the corresponding span's innerHTML.
+
+      var powerMetal = {
+        form:     document.power,
+        fields:   this.form.getElementsByTagName("input"),
+        spans:    [],
+        assignValue: function(){
+          // set innerHTML of spans to the value of each field
+        }
+      };
+
+      // old explanation:
+      // for each form (for each form name?), store the value of each field in
+      // one array, and store the corresponding <span> tag in another array.
+      // Then, assign the value of each field to the corresponding <span>'s
+      // innerHTML.
 
 
-
-
-
+      */
       return false;
     }
 
