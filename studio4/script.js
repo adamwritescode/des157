c3f8d35b6769ec5bd4d0306327f47bd3039a1284
adamwritescode <adamrosado@gmail.com>
2017-03-26 22:36:06 -0700

    studio 4 assets

--- studio4/script.js
+++ studio4/script.js
@@ -3,6 +3,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
-    // all other js here
+    var timeMoney = document.getElementById('time-money');
+    var timePatience = document.getElementById('time-patience');
+    var timeBrings = document.getElementById('time-brings');
+    var lostTime = document.getElementById('lost-time');
+    var spaceTime = document.getElementById('space-time');
+
 
 });
